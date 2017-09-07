ba3bce3e1bfefd6501d7ce3c58a8d3959a284da0
naveen arebailu <naveen.arebail@gmail.com>
2017-09-07 23:39:08 +0530

    files added

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '';
+  name= '';
+
+  ngOnInit(){
+
+    this.title='Interpolation example';
+    this.name='Naveen';
+  }
+
+}
+
